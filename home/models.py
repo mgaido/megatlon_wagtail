345e147dd59259cbb64b174548e75e5392eec556
mgaido <gaidomatias@gmail.com>
2023-11-27 17:48:13 -0300

    publish

---
+++ home/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+
+from modelcluster.fields import ParentalKey
+
+from wagtail.models import Page, Orderable
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel, InlinePanel
+from wagtail.search import index
+from taggit.models import TaggedItemBase
+from modelcluster.fields import ParentalKey
+from modelcluster.contrib.taggit import ClusterTaggableManager
+
+
+
+class HomePage(Page):
+    template = "home/home_page.html"
+
+    body = RichTextField(features=["bold", "italic"])
+    date = models.DateField("Post date", null=True)
+    feed_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body'),
+        FieldPanel('date'),
+        FieldPanel('feed_image'),
+    ]
+
+
+class BlogPageTag(TaggedItemBase):
+    content_object = ParentalKey('home.Pepe', on_delete=models.CASCADE, related_name='tagged_items')
+
+
+class Pepe(Page):
+    template = "home/home_page.html"
+
+    body = RichTextField(features=["bold", "italic"])
+    date = models.DateField("Post date", null=True)
+    tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
+    feed_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body'),
+        FieldPanel('date'),
+        FieldPanel('feed_image'),
+        FieldPanel('tags'),
+    ]
+
+
